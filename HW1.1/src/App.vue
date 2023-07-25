e989656dd88e9c878a497f470830088089dc56f8
MaxDosJS <122891450+MaxDosJS@users.noreply.github.com>
2023-07-25 11:39:58 +0600

    Add files via upload

---
+++ HW1.1/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <RandomNum />
+  </div>
+</template>
+
+<script setup>
+import RandomNum from '@/components/Randomizer.vue'
+</script>
+
+<style scoped></style>
---
+++ HW1.1/src/components/Randomizer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Rundom num: {{ randomNum }}</p>
+    <input id="end" type="number" placeholder="end">
+    <button @click="startRandom">GO</button>
+  </div>
+</template>
+
+<script setup>
+    import {ref} from "vue"
+    let randomNum = ref()
+    const history = []
+    const startRandom = () =>{
+      let endpoint = +document.querySelector('#end').value
+      let random = Math.floor(Math.random()*endpoint)
+      randomNum.value = random
+      history.push(random)
+      console.log(history)
+    }
+</script>
+
+<style scoped></style>
